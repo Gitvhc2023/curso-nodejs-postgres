a776b7a7a3c60b2d32b86e67cc77ce0ffbc07917
Gitvhc2023 <vcabezas2007@gmail.com>
2024-05-28 19:14:21 -0500

    clase18

--- services/product.service.js
+++ services/product.service.js
@@ -1,11 +1,16 @@
 const { faker } = require('@faker-js/faker');
 const boom = require('@hapi/boom');
 
+//const pool = require('../libs/postgres.pool');
+const sequelize = require('../libs/sequelize');
+
 class ProductsService {
 
   constructor(){
     this.products = [];
     this.generate();
+    //this.pool = pool;
+    //this.pool.on('error', (err) => console.error(err));
   }
 
   generate() {
@@ -30,8 +35,14 @@ class ProductsService {
     return newProduct;
   }
 
-  find() {
-    return this.products;
+  async find() {
+    const query = 'SELECT * FROM task';
+    const [data,metadata] = await sequelize.query(query);
+    return {data,metadata};
+    // const [data] = await sequelize.query(query);
+    // return data;
+    // const rta = await this.pool.query(query);
+    // return rta.rows;
   }
 
   async findOne(id) {
--- services/user.service.js
+++ services/user.service.js
@@ -1,29 +1,49 @@
 const boom = require('@hapi/boom');
 
+// const getConnection = require('../libs/postgres');
+const { models } = require('./../libs/sequelize');
+
 class UserService {
   constructor() {}
 
   async create(data) {
-    return data;
+    const newUser = await models.User.create(data);
+    return newUser;
+    // return data;
   }
 
   async find() {
-    return [];
+    const rta = await models.User.findAll();
+    return rta;
+    // const client = await getConnection();
+    // const rta = await client.query('SELECT * FROM task');
+    // return rta.rows;
   }
 
   async findOne(id) {
-    return { id };
+    const user = await models.User.findByPk(id);
+    if (!user) {
+      throw boom.notFound('user not found');
+    }
+    return user;
+    // return { id };
   }
 
   async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
+    const user = await this.findOne(id);
+    const rta = await user.update(changes);
+    return rta;
+    // return {
+    //   id,
+    //   changes,
+    // };
   }
 
   async delete(id) {
+    const user = await this.findOne(id);
+    await user.destroy();
     return { id };
+    // return { id };
   }
 }
 
